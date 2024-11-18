a7584cf571ee657d181d730c4e7b5fe142bd60fd
Roman Krendyasov <krendyasov_rn@mail.ru>
2024-11-18 18:24:49 +0400

    initial commit

--- InetionalSquare/ViewController.swift
+++ InetionalSquare/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var squareView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBlue
+        view.layer.cornerRadius = 8
+        view.frame = .init(origin: view.center, size: CGSize(width: 100, height: 100))
+        return view
+    }()
+    
+    private lazy var dynamicAnimator = UIDynamicAnimator(referenceView: view)
+    private lazy var snapBehavior = UISnapBehavior(item: squareView, snapTo: view.center)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(squareView)
+        dynamicAnimator.addBehavior(snapBehavior)
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touchCoordinates = touches.first?.location(in: view) else { return }
+        snapBehavior.snapPoint = touchCoordinates
+    }
 }
 
+#Preview {
+    ViewController()
+}
