dc748b26f1314a2ca0ef8b4a0c93a92995784091
Roman Krendyasov <krendyasov_rn@mail.ru>
2024-11-18 18:01:54 +0400

    Initial Commit

---
+++ InetionalSquare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InetionalSquare
+//
+//  Created by Роман Крендясов on 18.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
